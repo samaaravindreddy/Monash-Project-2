d9bee13a3761a1a775dbf2adadd30bce662b614a
gigi8android <gigi8android@gmail.com>
2022-02-08 16:44:07 +1100

    AH

--- query_ah.sql
+++ query_ah.sql
@@ -1,7 +1,9 @@
+------------ Drop table if exists ------------
 DROP TABLE IF EXISTS uni_rank;
 DROP TABLE IF EXISTS uni_details;
+----------------------------------------------
 
--- Create Two Tables
+--------- Create tables to hold data ---------
 CREATE TABLE uni_rank (
   id INT PRIMARY KEY,
   year INT,	
@@ -16,3 +18,14 @@ CREATE TABLE uni_details (
   country TEXT,
   region TEXT
 );
+----------------------------------------------
+
+------ Execute queries to check data load ------
+select * from uni_rank;
+select * from uni_details;
+
+select uni_rank.year, uni_rank.rank, uni_rank.score, uni_details.university, uni_details.city, uni_details.country, uni_details.region
+from uni_details
+inner join uni_rank
+on uni_details.id = uni_rank.id;
+----------------------------------------------
